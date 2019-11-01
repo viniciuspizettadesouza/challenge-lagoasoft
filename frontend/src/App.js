a3e7bd658f1d1b5a867ce648ddec4b4b082bdf32
DELL <viniciuspzt@gmail.com>
2019-11-01 15:50:19 -0300

    remove component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 
 import Header from './components/Header'
 import Post from './components/Post'
 
-class App extends Component {
-  render() {
-    return <div className="App">
+
+export default function App() {
+  return (
+    <div className="App">
       <Header />
       <Post />
     </div>
-  }
+  )
 }
-export default App
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,19 +2,17 @@ import React from "react"
 
 import "./Header.css"
 
-class Header extends React.Component {
-    render() {
-        return (
-            <nav className="Nav">
-                <div className="Nav-menus">
-                    <div className="Nav-brand">
-                        <a className="Nav-brand-logo" href="/">
-                            Instagram
+const Header = () => {
+    return (
+        <nav className="Nav">
+            <div className="Nav-menus">
+                <div className="Nav-brand">
+                    <a className="Nav-brand-logo" href="/">
+                        Instagram
                         </a>
-                    </div>
                 </div>
-            </nav>
-        )
-    }
+            </div>
+        </nav>
+    )
 }
 export default Header
