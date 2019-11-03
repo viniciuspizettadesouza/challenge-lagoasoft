12db6b7083850396b83bf1d6fa3e2433191050d5
DELL <viniciuspzt@gmail.com>
2019-11-03 12:13:26 -0300

    new component like button

--- frontend/src/components/Post/index.js
+++ frontend/src/components/Post/index.js
@@ -1,51 +1,18 @@
-import React, { useState } from "react"
-
-import "./Post.css"
+import React from "react"
 
 import customData from "../../data/customData"
 
-import like from '../../assets/like.svg'
+import OnePost from './onePost'
 
 const Post = () => {
 
-    const [likes, setLikes] = useState(0)
-
     return (
-        <div>
+        <>
             {customData.map(post => (
-                <div className="Post" key={post.id}>
-                    <div className="Post-user">
-                        <div className="Post-user-avatar">
-                            <img src={post.avatar} alt={post.nickname} />
-                        </div>
-                        <div className="Post-user-nickname">
-                            <p>{post.nickname}</p>
-                        </div>
-                    </div>
-                    <div className="Post-image">
-                        <div className="Post-image-bg">
-                            <img alt={post.caption} src={post.image} />
-                        </div>
-                    </div>
-                    <div className="post-footer">
-                        <button className="button" type="button"
-                            onClick={() => setLikes(post.likes = post.likes + 1)}
-                        >
-                            <img src={like} alt="Like" />
-                        </button>
-                        <p className="Post-caption">Curtido por <a className="Post-likes" href="/">{post.likes}</a> pessoas</p>
-                    </div>
-                    <div className="Post-description">
-                        <p className="Post-user-nickname">{post.nickname}</p>
-                        <p className="Post-caption">{post.caption}</p>
-                    </div>
-                    <div className="Post-date">
-                        <p>{post.date}</p>
-                    </div>
-                </div>
+                <OnePost post={post} key={post.id} />
             ))
             }
-        </div>
+        </>
     )
 }
 export default Post
